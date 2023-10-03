beb53d08a76bb43b914b67f801036b419998f1eb
mumunin <146727285+mumunin@users.noreply.github.com>
2023-10-03 12:14:39 +0700

    Add files via upload

---
+++ duzn.js
@@ -0,0 +1,94 @@
+function sc1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=e76ae17d861546be9c238e6582509c2c&key=de1a91e2306bf8442236a80477f526e1&rasio=exactfi&file=https://ssc-1-on-prem-ak.akamaized.net/out/v1/ec938957da2849879f0cfac7e309ff38/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sc2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=b8763ccad3d146c2b15f8a77a4d6d733&key=ab87f6ae46b2dc187bd94cbb48cfe074&rasio=exactfi&file=https://ssc-2-on-prem-ak.akamaized.net/out/v1/d9a2acf5f809461ca47714440fcbc0f4/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function sc3(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c3af2aa1da4a41cbb37b1dbe43b46782&key=b6b74f29f6d1501b989c96aec4dae599&rasio=exactfi&file=https://ssc-3-on-prem-ak.akamaized.net/out/v1/ad2b1abef05c419aa6a4e6aee6d269a2/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function sc4(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=2c506baa39a74674874ae2ab8bbc792f&key=1fa11c606eea3ba805f3d2efa537b1fa&rasio=exactfi&file=https://ssc-4-on-prem-ak.akamaized.net/out/v1/3e442c7dad1b44b6a0914aa26b62a892/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function sc5(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=150150f30a8140a2bfda5d1b3fbb2d4b&key=dad19250368b129f003817b778d65dae&rasio=exactfi&file=https://ssc-5-on-prem-cw-ak.akamaized.net/out/v1/ee54cd9a3f844ceb8249f430733f9c00/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function sc1x(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=0bf5a32868224a6f9f3a749a9ea20eb2&key=0ace749c97932aa44434da3a88c1ee69&rasio=exactfi&file=https://ssc-extra1-on-prem-cw-ak.akamaized.net/out/v1/d309ac98b822425d9435cca654211498/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function sc2x(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=fc98baf530ac49aea60a7bf50929aa91&key=f8c40722bb6a0cf822588661740a08bb&rasio=exactfi&file=https://ssc-extra2-on-prem-ak.akamaized.net/out/v1/a525c44d152f4d238aa21af75945e29c/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function sc3x(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=9b05d5f11a0b422d86354c34782ed2bc&key=82aef319b4cfc71fd48cdd495ee79786&rasio=exactfi&file=https://ssc-extra3-on-prem-cw-ak.akamaized.net/out/v1/9d373d000ab64078b131f9d4c18e3502/index.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+
+
+function zig1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=90e32a4712f5480bab7abc8a4fef25da&key=2eeb49a8688f2365e1131cf4ae4d6f0a&file=https://wp-obc4-live-nl-prod.prod.cdn.dmdsdp.com/dash/go-dash-hdready-avc/NL_000014_019661/manifest.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function zig2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=25586a5ca20847d684579cce0c2ecacc&key=9c92162b23709653688a471bec0cf19b&file=https://da-d436234a20010b8800010302000000000000000c.id.cdn.upcbroadband.com/wp/wp-obc1-live-nl-prod.prod.cdn.dmdsdp.com/dash/NL_000095_019371/manifest.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+  
+function ps1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-051/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function ps2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-052/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function ps3(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-053/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function elve1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-054/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function elve2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-055/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function elve3(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-056/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function elve4(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-057/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function elve5(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-058/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function elve6(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-059/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+  
+function dzn1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-037/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function dzn2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-058/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function dzn1de(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-017/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function dzn2de(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-018/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function dznlg(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-043/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+  
+function ben1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-039/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function ben2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-040/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function ben3(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-041/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function digi(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-022/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function dzon(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-024/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function dzf1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-016/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function dzba(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-035/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function mlb(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-026/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function nfl(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-023/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function rbtv(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=82f8e2a17dac44c0a18f660479349c59&key=1b144f73e6fefe91cd05f850e2b589d0&rasio=exactfi&file=https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-036/stream.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+  
+function sar1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/as_premium1.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sar2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/as_premium2.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sar3(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/as_premium3.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sas1(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/arena1.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sas2(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/arena2.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sas3(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/arena3.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sas4(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/arena4.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sas5(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/arena5.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+function sas6(){document.getElementById("evt")
+.innerHTML='<iframe src="/2023/07/player3.html?keyid=c18b6aa739be4c0b774605fcfb5d6b68&key=e41c3a6f7532b2e3a828d9580124c89d&rasio=exactfi&file=https://webtvstream.bhtelecom.ba/hls7/arena6.mpd" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}  
+
+  
